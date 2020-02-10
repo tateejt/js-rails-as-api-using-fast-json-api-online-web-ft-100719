915c4f436886c0c83b34243509f37c014ad26437
Emery Tate <tateejt@gmail.com>
2020-02-10 01:49:57 +0000

    Automatically backed up by Learn

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,4 +1,6 @@
 class SightingSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :create_at, :bird, :location
+  attributes :create_at
+  belongs_to :bird
+  belongs_to :location
 end
